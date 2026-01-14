cc7154089e03c8c5fbb44b00a4f45a27defd8e7c
Michal Kolodziej <m.kolodziej2211@gmail.com>
2026-01-14 15:04:55 +0100

    added log in with email and password

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,7 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 abstract interface class AuthService {
   Future<void> initialize();
-  Future<void> logIn();
+  Future<void> googleLogIn();
+  Future<void> logIn(String email, String password);
   Future<void> logOut();
   User? get user;
 }
