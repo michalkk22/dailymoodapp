86ac897282f787887103f4a85d629c76315938f8
Michal Kolodziej <m.kolodziej2211@gmail.com>
2026-01-14 16:32:11 +0100

    added responses states and events

---
+++ lib/bloc/responses/responses_bloc.dart
@@ -0,0 +1,6 @@
+import 'package:class_pulse/data/models/response.dart';
+import 'package:class_pulse/data/models/session.dart';
+import 'package:flutter/foundation.dart' show immutable;
+
+part 'responses_event.dart';
+part 'responses_state.dart';
