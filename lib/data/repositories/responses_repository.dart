5979b8c5ceebac68e7fc24feb8d0230e5f030713
agaiffe2-iut90 <anna.gaiffe@edu.univ-fcomte.fr>
2026-01-14 16:15:07 +0100

    responses repository

---
+++ lib/data/repositories/responses_repository.dart
@@ -0,0 +1,9 @@
+import '../models/response.dart';
+
+abstract interface class ResponsesRepository {
+  Stream<List<Response>> responses(String sessionId);
+
+  Future<void> add(String sessionId, Response response);
+
+  Future<void> delete(String sessionId, Response response);
+}
